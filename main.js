777b655626588d54a7884fd129cbb2044ed49e18
rafaelpoluha <163421927+rafaelpoluha@users.noreply.github.com>
2025-06-02 14:23:15 +0000

    ID

--- main.js
+++ main.js
@@ -37,13 +37,21 @@ function calculaTempo(tempoObjetivo){
     minutos %= 60;
     horas %= 24;
 
-    return dias + "dias "+ horas + "horas" + minutos + "minutos" + segundos + "segundos";
+    if (tempoFinal >0) {  
+        return [dias, horas,  minutos, segundos];
+    }else{
+        return[0, 0, 0, 0];
+    }
 }
 
 function atualizarCronometro(){
     for(let i= 0; i < contadores.length; i++){
-        contadores[i].textContent = calculaTempo(tempo[i]);
-    }
+    document.getElementById("dias" + i).textContent = calculaTempo(tempos[i])[0];
+    document.getElementById("dias" + i).textContent = calculaTempo(tempos[i])[1];
+    document.getElementById("dias" + i).textContent = calculaTempo(tempos[i])[2];
+    document.getElementById("dias" + i).textContent = calculaTempo(tempos[i])[3];
+        
+}
 }
 function comecaCronometro(){
     atualizarCronometro();
